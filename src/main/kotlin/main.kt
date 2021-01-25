29ec7430d1c38fdd7b2336017e8b81d95dd042a7
Natalia Gvozdeva <gvozdeva-natalia@mail.ru>
2021-01-25 19:49:56 +0300

    Made corrections to improve code structure

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -17,7 +17,6 @@ fun main() {
 
     println("Введите тип перевода: \nMastercard - 'mc',\nMaestro - 'mo',\nVisa - 'v',\nMir - 'm',\nVKPay - 'vk' ")
     val typeOfCardOrAccount: String? = readLine()
-    println("Тип перевода: $typeOfCardOrAccount")
 
     if ((currentExecutedAmount != null) && (currentDayAmount != null) &&
         (currentMonthAmount != null) && (typeOfCardOrAccount != null)
@@ -35,11 +34,11 @@ fun main() {
             }
         }
 
-        if (checkTransferLimits(fullNameOfTransferType, currentDayAmount, currentMonthAmount, currentExecutedAmount)) {
+        commission = transfer(fullNameOfTransferType, currentDayAmount, currentMonthAmount, currentExecutedAmount)
+
+        if (commission == -1) {
             return
         }
-
-        commission = transfer(fullNameOfTransferType, currentMonthAmount, currentExecutedAmount)
 
         println("Перевод c $fullNameOfTransferType выполнится с комиссий: $commission копеек")
 
@@ -50,78 +49,113 @@ fun main() {
 
 }
 
-fun checkTransferLimits(
+fun checkVkPayLimits(
     fullNameOfTransferType: String,
     currentDayAmount: Int,
     currentMonthAmount: Int,
     currentExecutedAmount: Int
-): Boolean {
-    val cardDayLimit = 150_000 * 100
-    val cardMonthLimit = 600_000 * 100
+): Int {
+
     val vkPayMonthLimit = 40_000 * 100
     val vkPayOnceLimit = 15_000 * 100
-    val cardVisaMirMinimum = 35 * 100
 
-    when (fullNameOfTransferType) {
-        "Mastercard", "Maestro", "Visa", "Mir" -> {
-            when {
-                (((fullNameOfTransferType == "Visa" )||(fullNameOfTransferType == "Mir" ))&&(currentExecutedAmount < cardVisaMirMinimum)) -> {
-                    println("По карте $fullNameOfTransferType сумма перевода меньше минимального лимита $cardVisaMirMinimum.")
-                    return true
-                }
-                ((currentDayAmount + currentExecutedAmount) > cardDayLimit) -> {
-                    println("По карте $fullNameOfTransferType превышен суточный лимит")
-                    return true
-                }
-                ((currentMonthAmount + currentDayAmount + currentExecutedAmount) > cardMonthLimit) -> {
-                    println("По карте $fullNameOfTransferType превышен месячный лимит")
-                    return true
-                }
-            }
-            return false
+    when {
+        (currentExecutedAmount > vkPayOnceLimit) -> {
+            println("По карте $fullNameOfTransferType превышен разовый лимит")
+            return -1
         }
-        "VkPay" -> {
-            when {
-                (currentExecutedAmount > vkPayOnceLimit) -> {
-                    println("По карте $fullNameOfTransferType превышен разовый лимит")
-                    return true
-                }
-                ((currentMonthAmount + currentDayAmount + currentExecutedAmount) > vkPayMonthLimit) -> {
-                    println("По карте $fullNameOfTransferType превышен месячный лимит")
-                    return true
-                }
-            }
-            return false
+        ((currentMonthAmount + currentDayAmount + currentExecutedAmount) > vkPayMonthLimit) -> {
+            println("По карте $fullNameOfTransferType превышен месячный лимит")
+            return -1
         }
-        else -> return false
     }
+    return 0
 }
 
 
 fun transferMasterMaestro(
+    typeOfCardOrAccount: String,
     currentMonthAmount: Int = 0,
+    currentDayAmount: Int = 0,
     currentExecutedAmount: Int = 0
 ): Int {
 
+    val cardDayLimit = 150_000 * 100
+    val cardMonthLimit = 600_000 * 100
     val maxAdvActionLimit = 75000 * 100
 
-    if (currentMonthAmount <= maxAdvActionLimit) {
+    when {
+        ((currentDayAmount + currentExecutedAmount) > cardDayLimit) -> {
+            println("По карте $typeOfCardOrAccount превышен суточный лимит")
+            return -1
+        }
+        ((currentMonthAmount + currentDayAmount + currentExecutedAmount) > cardMonthLimit) -> {
+            println("По карте $typeOfCardOrAccount превышен месячный лимит")
+            return -1
+        }
+    }
+
+    if ((currentMonthAmount + currentDayAmount + currentExecutedAmount) <= maxAdvActionLimit) {
         return 0
     } else {
-        return (currentExecutedAmount * 6 / 1000 + 20 * 100)
+        return (currentExecutedAmount * 6 / 1000 + 20 * 100).toInt()
     }
 }
 
 fun transferVisaMir(
+    typeOfCardOrAccount: String,
+    currentMonthAmount: Int = 0,
+    currentDayAmount: Int = 0,
     currentExecutedAmount: Int = 0
-): Int = (currentExecutedAmount * 75 / 10000)
+): Int {
+
+    val cardDayLimit = 150_000 * 100
+    val cardMonthLimit = 600_000 * 100
+    val cardVisaMirMinimum = 35 * 100
+
+    when {
+        (currentExecutedAmount < cardVisaMirMinimum) -> {
+            println("По карте $typeOfCardOrAccount сумма перевода меньше минимального лимита $cardVisaMirMinimum.")
+            return -1
+        }
+        ((currentDayAmount + currentExecutedAmount) > cardDayLimit) -> {
+            println("По карте $typeOfCardOrAccount превышен суточный лимит")
+            return -1
+        }
+        ((currentMonthAmount + currentDayAmount + currentExecutedAmount) > cardMonthLimit) -> {
+            println("По карте $typeOfCardOrAccount превышен месячный лимит")
+            return -1
+        }
+    }
+
+    return (currentExecutedAmount * 75 / 10000).toInt()
+}
 
 fun transfer(
     typeOfCardOrAccount: String = "VkPay",
+    currentDayAmount: Int = 0,
     currentMonthAmount: Int = 0,
     currentExecutedAmount: Int = 0
-): Int = when (typeOfCardOrAccount) {
-    "Mastercard", "Maestro" -> transferMasterMaestro(currentMonthAmount + currentExecutedAmount, currentExecutedAmount)
-    "Visa", "Mir" -> transferVisaMir(currentExecutedAmount)
-    else -> 0
+): Int {
+    val comission = when (typeOfCardOrAccount) {
+        "Mastercard", "Maestro" -> transferMasterMaestro(
+            typeOfCardOrAccount,
+            currentMonthAmount,
+            currentDayAmount,
+            currentExecutedAmount
+        )
+        "Visa", "Mir" -> transferVisaMir(
+            typeOfCardOrAccount,
+            currentMonthAmount,
+            currentDayAmount,
+            currentExecutedAmount
+        )
+        else -> checkVkPayLimits(
+            typeOfCardOrAccount,
+            currentMonthAmount,
+            currentDayAmount,
+            currentExecutedAmount
+        )
+    }
+    return comission
 }
