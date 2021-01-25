3aaca24a8918e9f209b223199f573cce4d320911
Natalia Gvozdeva <gvozdeva-natalia@mail.ru>
2021-01-25 17:21:32 +0300

    There was deleted draft comments in main.kt module

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,29 +4,6 @@ fun main() {
 
     val commission: Int
 
-//    val amount1 = 300 * 100    // для карты комиссия 225к
-//    val amount2 = 27000 * 100  // для карты комиссия 20250к, VK Pay - привышение лимита
-//    val amount3 = 35 * 100     // для карты комиссия 26к
-//    val amount4 = 15 * 100     // для карты - меньше допустимого значения
-//    val amount5 = 150001 * 100 // для карты комиссия - привышение лимита, VK Pay - привышение лимита
-//    val amount6 = 600001 * 100 // для карты комиссия - привышение лимита, VK Pay - привышение лимита
-//    val amount7 = 15001 * 100  // для карты комиссия 11250к, VK Pay - привышение лимита
-//    val amount8 = 40001 * 100  // для карты комиссия 30000к, VK Pay - привышение лимита
-
-//    val minCardAmount = 35 * 100
-//    val minVkpayAmount = 0
-//
-//    val cardDayLimit = 150_000 * 100
-//    val vkPayDayLimit = 15_000 * 100
-//
-//    val typeMasterMaestro = 1
-//    val typeVisaMir = 2
-//    val typeVKPay = 3
-
-    /*!!!! Здесь можно подставлять подходящее значения !!!*/
-//    val transferType = 1 /*Mastercard,Maestro - 1; Visa,Мир - 2; VK Pay - 3;*/
-//    val currentExecutedAmount = amount1 /* <--- сумма перевода */
-
     println("Введите сумму перевода в рублях")
     val currentExecutedAmount = readLine()!!.toInt() * 100
     println("Сумма перевода: $currentExecutedAmount")
@@ -45,7 +22,6 @@ fun main() {
     if ((currentExecutedAmount != null) && (currentDayAmount != null) &&
         (currentMonthAmount != null) && (typeOfCardOrAccount != null)
     ) {
-
 
         val fullNameOfTransferType = when (typeOfCardOrAccount) {
             "mc" -> "Mastercard"
@@ -134,7 +110,6 @@ fun transferMasterMaestro(
     } else {
         return (currentExecutedAmount * 6 / 1000 + 20 * 100)
     }
-
 }
 
 fun transferVisaMir(
